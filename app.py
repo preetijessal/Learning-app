105618805d7f1312d5e13e8c07ae172b05f2739f
Sehajpreet jessal <31192959+preetijessal@users.noreply.github.com>
2026-05-30 17:18:48 -0400

    Remove division_multiplication route from app.py

--- app.py
+++ app.py
@@ -1343,9 +1343,7 @@ def codelab():
     return render_template("codelab.html")
 
 
-@app.route("/division-multiplication")
-def division_multiplication():
-    return render_template("division_multiplication.html")
+
 
 
 if __name__ == "__main__":

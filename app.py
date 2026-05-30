7829e3a2933d677c8403f186b6e46acbcd62be1e
Sehajpreet jessal <31192959+preetijessal@users.noreply.github.com>
2026-05-30 17:22:04 -0400

    Add new routes for learn and division pages

--- app.py
+++ app.py
@@ -1343,7 +1343,14 @@ def codelab():
     return render_template("codelab.html")
 
 
+@app.route("/learn")
+def learn_to_read():
+    return render_template("learn.html")
 
+
+@app.route("/division-multiplication")
+def division_multiplication():
+    return render_template("division_multiplication.html")
 
 
 if __name__ == "__main__":
